5fd2e4ee3f308b41125091ce5db826aab7cf4d4e
salmonmoose <salmonmoose@gmail.com>
2012-03-06 15:41:29 +1030

    that's better

--- pymine.py
+++ pymine.py
@@ -6,9 +6,9 @@ d8888b. db    db .88b  d88. d888888b d8b   db d88888b
 88         88    88  88  88   .88.   88  V888 88.     
 88         YP    YP  YP  YP Y888888P VP   V8P Y88888P 
 '''
-from moosecore import Game
-import mooselog
-import gamemap
+from bin.moosecore.moosecore import Game
+import bin.moosecore.mooselog
+import bin.moosecore.gamemap
 
 def main():
         game = Game()
