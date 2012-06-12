58db9eafac611c74b9a110806a31a9e465ace56c
Anton Fletcher <salmonmoose@gmail.com>
2012-06-12 15:45:00 +0930

    Just a bit of cleanup

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from distutils.core import setup, Extension
 
 ext = Extension('marchingcubes', sources=['marchingcubesmodule.c'])
 
-setup(name='marchingcubes', version='1.0', description='Test description', ext_modules=[ext])
+setup(name='marchingcubes', version='1.0', description='Generates triangle set from volume data', ext_modules=[ext])
