5d2644f7cd0c63065724e7ace07fbecc747589ae
Anton Fletcher <salmonmoose@gmail.com>
2012-08-17 17:44:44 +0930

    caching vertex creation

--- src/marchingcubes.cpp
+++ src/marchingcubes.cpp
@@ -51,14 +51,23 @@ int zDim = tc.values.shape()[2];
 int x,y,z,i,cubeIndex,cacheIndex,ntriang;
 float pointVals[8];
 int colorVals[8];
-video::S3DVertex vertList[12];
+int vertList[12];
+video::S3DVertex tmpS3DVertex;
+int vertIndexList[12];
+
 float isolevel = 0.5;
 float mu;
-    core::vector3df tmpVec3D;
 
 int uSpace = yDim * zDim * (xDim -1);
 int vSpace = zDim * xDim * (yDim -1);
 int wSpace = xDim * yDim * (zDim -1);
+
+int index;
+
+core::vector3df tmpVec3D;
+int generatedPoints[uSpace + vSpace + wSpace];
+
+std::fill_n(generatedPoints, uSpace + vSpace + wSpace, -1);
 
 scene::SMeshBuffer *buf = 0;
 
@@ -70,6 +79,7 @@ buf->drop();
 buf->Vertices.clear();
 buf->Indices.clear();
 
+
 for (z = 0; z < zDim -1; z++) {
     for (y = 0; y < yDim -1; y++) {
         for (x = 0; x < xDim -1; x++) {
@@ -80,43 +90,60 @@ for (z = 0; z < zDim -1; z++) {
                 colorVals[i] = tc.materials[x + (int)points[i].X][y + (int)points[i].Y][z + (int)points[i].Z];
                 if(pointVals[i] > isolevel) cubeIndex |= (1 << i);
             }
-            tmpVec3D = core::vector3df((double) x, (double) y, (double) z);
+            
 
-                //Generate verts needed by this voxel.
+            //Generate verts needed by this voxel.
             for (i = 0; i < 12; i++) {
-                if (edgeTable[cubeIndex] & (1 << i))
-                {
-                    printf("pointvals %f %f\n", pointVals[edges[i][0]], pointVals[edges[i][1]]);
+                switch (edges[i][2]) {
+                    case 0: //Change in X direction
+                        index = ((int)points[edges[i][0]].X + x) + 
+                               (((int)points[edges[i][0]].Y + y) * (xDim-1)) + 
+                               (((int)points[edges[i][0]].Z + z) * (xDim-1)  * yDim);
+                    break;
+                    case 1: //Change in Y direction
+                        index = ((int)points[edges[i][0]].X + x) + 
+                               (((int)points[edges[i][0]].Y + y) * xDim) + 
+                               (((int)points[edges[i][0]].Z + z) * xDim  * (yDim-1)) +
+                               uSpace;
+                    break;
+                    case 2: //Change in Z direction
+                        index = ((int)points[edges[i][0]].X + x) + 
+                               (((int)points[edges[i][0]].Y + y) * xDim) + 
+                               (((int)points[edges[i][0]].Z + z) * xDim  * yDim) +
+                               uSpace + vSpace;
+                    break;
+                }
 
+                if(edgeTable[cubeIndex] & (1 << i)) {
+                    if (generatedPoints[index] == -1) {
+                        mu = (isolevel - pointVals[edges[i][0]]) / (pointVals[edges[i][1]] - pointVals[edges[i][0]]);
+                        
+                        tmpS3DVertex.Pos.set(
+                            x + (points[edges[i][0]].X + mu * (points[edges[i][1]].X - points[edges[i][0]].X)),
+                            y + (points[edges[i][0]].Y + mu * (points[edges[i][1]].Y - points[edges[i][0]].Y)),
+                            z + (points[edges[i][0]].Z + mu * (points[edges[i][1]].Z - points[edges[i][0]].Z))
+                            );
 
-                    mu = (isolevel - pointVals[edges[i][0]]) / (pointVals[edges[i][1]] - pointVals[edges[i][0]]);
+                        tmpS3DVertex.Color.set(
+                            colors[colorVals[edges[i][0]]].r + mu * (colors[colorVals[edges[i][1]]].r - colors[colorVals[edges[i][0]]].r),
+                            colors[colorVals[edges[i][0]]].g + mu * (colors[colorVals[edges[i][1]]].g - colors[colorVals[edges[i][0]]].g),
+                            colors[colorVals[edges[i][0]]].b + mu * (colors[colorVals[edges[i][1]]].b - colors[colorVals[edges[i][0]]].b),
+                            colors[colorVals[edges[i][0]]].a + mu * (colors[colorVals[edges[i][1]]].a - colors[colorVals[edges[i][0]]].a)
+                            );
+
+                        tmpS3DVertex.TCoords.set(0.0,0.0);
+
+                        generatedPoints[index] = buf->Vertices.size();
+                        buf->Vertices.push_back(tmpS3DVertex); //FIXME this should just build the vertex here and now.
+                    }
                     
-                    vertList[i].Pos.set(
-                        tmpVec3D.X + (points[edges[i][0]].X + mu * (points[edges[i][1]].X - points[edges[i][0]].X)),
-                        tmpVec3D.Y + (points[edges[i][0]].Y + mu * (points[edges[i][1]].Y - points[edges[i][0]].Y)),
-                        tmpVec3D.Z + (points[edges[i][0]].Z + mu * (points[edges[i][1]].Z - points[edges[i][0]].Z))
-                        );
-
-                    vertList[i].Color.set(
-                        colors[colorVals[edges[i][0]]].r + mu * (colors[colorVals[edges[i][1]]].r - colors[colorVals[edges[i][0]]].r),
-                        colors[colorVals[edges[i][0]]].g + mu * (colors[colorVals[edges[i][1]]].g - colors[colorVals[edges[i][0]]].g),
-                        colors[colorVals[edges[i][0]]].b + mu * (colors[colorVals[edges[i][1]]].b - colors[colorVals[edges[i][0]]].b),
-                        colors[colorVals[edges[i][0]]].a + mu * (colors[colorVals[edges[i][1]]].a - colors[colorVals[edges[i][0]]].a)
-                        );
-                    
-                    vertList[i].TCoords.set(0.0,0.0);
-
-                    printf("added point at (%f, %f, %f)\n",
-                        vertList[i].Pos.X,
-                        vertList[i].Pos.Y,
-                        vertList[i].Pos.Z);
+                    vertList[i] = index;
                 }
             }
 
-                for (i=0;triTable[cubeIndex][i]!=-1;i++) {
-                    buf->Indices.push_back(buf->Vertices.size());
-                    buf->Vertices.push_back(vertList[triTable[cubeIndex][i]]);
-                }
+            for (i=0;triTable[cubeIndex][i]!=-1;i++) {
+                buf->Indices.push_back(generatedPoints[vertList[triTable[cubeIndex][i]]]);
+            }
         }
     }
 }
